6be42561ce4519ba5dbb75af96905957498183bf
konsh <danik90011@gmail.com>
2025-10-06 21:09:56 +0300

    reformatted3.0

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -8,9 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int OLD_TASK = 0;
-    private static final int HISTORY_LIST_CAPACITY = 10;
-    private final List<Task> taskHistoryList = new ArrayList<>(HISTORY_LIST_CAPACITY);
     private final Map<Integer, Node> nodeMap = new HashMap<>();
     private Node first;
     private Node last;
