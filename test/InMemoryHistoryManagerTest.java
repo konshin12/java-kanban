e02317dedadcf9563e6c23461abffde66344328a
konsh <danik90011@gmail.com>
2025-10-09 16:21:14 +0300

    dobavil

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-class InMemoryHistoryManagerTest {
+public class InMemoryHistoryManagerTest {
     private InMemoryHistoryManager historyManager;
 
     @BeforeEach
